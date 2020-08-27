ef6f9ae97c6945cb0fe8acbc8efcfb09a6369775
Minh Nguyen <minh.nguyen@dsquare.com.vn>
2020-08-27 16:51:26 +0700

    change output

--- _config_all.php
+++ _config_all.php
@@ -6,9 +6,9 @@ $variableGit['PROJECT_NAME'] = str_replace('-','_',$variableGit['PROJECT_NAME'])
 
 $variableGit['GIT_SOURCE'] = sprintf('http://%s:%s@%s',$variableGit['GIT_USER_NAME'],$variableGit['GIT_PASSWORD'],$variableGit['GIT_URL']);
 
-$output = shell_exec(sprintf('bash /dsquare/docker/docker.git.sh --PROJECT_NAME=%s --GIT_SOURCE=%s --GIT_BRANCH=%s', $variableGit['PROJECT_NAME'],$variableGit['GIT_SOURCE'],$variableGit['GIT_BRANCH']));
+$runGit = shell_exec(sprintf('bash /dsquare/docker/docker.git.sh --PROJECT_NAME=%s --GIT_SOURCE=%s --GIT_BRANCH=%s', $variableGit['PROJECT_NAME'],$variableGit['GIT_SOURCE'],$variableGit['GIT_BRANCH']));
 
-echo $output;
+echo $runGit;
 
 $webConfig = shell_exec(sprintf('php /dsquare/docker/_web_config.php "%s" ', $variableGit['PROJECT_NAME']));
 
