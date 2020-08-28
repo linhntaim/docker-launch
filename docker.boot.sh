7d35a0f31106520024cd37c2bed5b8112b07167f
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2020-08-28 17:36:37 +0700

    docker done

--- docker.boot.sh
+++ docker.boot.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # Variable
 PROJECT_NAME="$@"
-echo "### PROJECT_NAME=${PROJECT_NAME}"
 # MySQL
 ### Set directory's permission; without it, service cannot start
+usermod -d /var/lib/mysql/ mysql
 chown -R mysql:mysql /var/lib/mysql
 ### Start service
 service mysql start
 ### Create database
 NONE_EXISTED=$(mysql -s -N -e "SELECT schema_name FROM information_schema.schemata where schema_name = '${PROJECT_NAME}';")
 if [[ -z "${NONE_EXISTED}" ]]; then
-mysql -e "CREATE DATABASE `${PROJECT_NAME}`;"
+mysql -e "CREATE DATABASE \`${PROJECT_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 ### Create new user for database
 mysql -e "CREATE USER '${PROJECT_NAME}'@'%' IDENTIFIED BY '${PROJECT_NAME}';"
 ### Make sure user can handle all actions on the database
-mysql -e "GRANT ALL ON `${PROJECT_NAME}`.* TO '${PROJECT_NAME}'@'%';" #
+mysql -e "GRANT ALL ON \`${PROJECT_NAME}\`.* TO '${PROJECT_NAME}'@'%';" #
 ### Apply the change of user permissions
 mysql -e "FLUSH PRIVILEGES;"
 ### Create database structure and data by script
-mysql -e "USE ${PROJECT_NAME};SOURCE /docker/resource/${PROJECT_NAME}.sql;"
+mysql -e "USE \`${PROJECT_NAME}\`;SOURCE /docker/resource/${PROJECT_NAME}.sql;"
 fi
 # Laravel
 ### Install packages
@@ -35,12 +35,12 @@ echo "* * * * * php /dsquare/${PROJECT_NAME}/artisan schedule:run >> /dev/null 2
 ##service sendmail start
 # Supervisor
 ### Configuration to run Laravel queue
-### cp /docker/.supervisor.conf /etc/supervisor/conf.d/${PROJECT_NAME}.conf
+cp /docker/.supervisor.conf /etc/supervisor/conf.d/${PROJECT_NAME}.conf
 ### Start service
-##  service supervisor start
+service supervisor start
 # PHP
 ### Start service
-service php7.3-fpm start
+service php7.4-fpm start
 # NGINX configuration
 ### Replace the configuration to run application
 cp /docker/.nginx.conf /etc/nginx/sites-available/default
